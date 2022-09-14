1b327f39484caf755d831a58769161f59d747c32
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-14 09:01:26 -0500

    Fullstack part6 : redux-notes

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 
 import { createStore } from 'redux';
 
-// reducer
+// reducers
 const counterReducer = (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
@@ -16,20 +16,47 @@ const counterReducer = (state = 0, action) => {
       return state;
   }
 };
+const noteReducer = (state = [], action) => {
+  if (action.type === 'NEW_NOTE') {
+    state.push(action.data);
+    return state;
+  }
+
+  return state;
+};
 // store
-const store = createStore(counterReducer);
+// const store = createStore(counterReducer);
+const store = createStore(noteReducer);
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'the app state is in redux store',
+    important: true,
+    id: 1,
+  },
+});
+
+store.dispatch({
+  type: 'NEW_NOTE',
+  data: {
+    content: 'state changes are made with actions',
+    important: false,
+    id: 2,
+  },
+});
+
 //  APP
 const App = () => {
   return (
     <div>
-      <div>{store.getState()}</div>
-      <button onClick={(e) => store.dispatch({ type: 'INCREMENT' })}>
-        plus
-      </button>
-      <button onClick={(e) => store.dispatch({ type: 'DECREMENT' })}>
-        minus
-      </button>
-      <button onClick={(e) => store.dispatch({ type: 'ZERO' })}>zero</button>
+      <ul>
+        {store.getState().map((note) => (
+          <li key={note.id}>
+            {note.content} <strong>{note.important ? 'important' : ''}</strong>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
@@ -40,3 +67,11 @@ const renderApp = () => {
 
 renderApp();
 store.subscribe(renderApp);
+
+// lastAPP
+// <div>
+//  <div>{store.getState()}</div>
+//  <button onClick={(e) => store.dispatch({ type: 'INCREMENT' })}>plus</button>
+//  <button onClick={(e) => store.dispatch({ type: 'DECREMENT' })}>minus</button>
+//  <button onClick={(e) => store.dispatch({ type: 'ZERO' })}>zero</button>
+// </div>;
