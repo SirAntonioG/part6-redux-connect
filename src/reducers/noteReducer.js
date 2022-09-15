084a16499096d21459556f5fd1522c69b0de430d
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-15 09:29:38 -0500

    Fullstack part6 section b : redux toolkit

--- src/reducers/noteReducer.js
+++ src/reducers/noteReducer.js
@@ -1,3 +1,5 @@
+import { createSlice } from '@reduxjs/toolkit';
+
 // inital state
 const initialState = [
   {
@@ -12,45 +14,31 @@ const initialState = [
   },
 ];
 
-// reducer
-const noteReducer = (state = initialState, action) => {
-  // console.log('STATE', state, 'ACTION', action);
-  switch (action.type) {
-    case 'NEW_NOTE':
-      return [...state, action.data];
-    case 'TOGGLE_IMPORTANCE': {
-      const id = action.data.id;
+const generateId = () => Number((Math.random() * 1000000).toFixed(0));
+// slice
+const noteSlice = createSlice({
+  name: 'notes',
+  initialState,
+  reducers: {
+    createNote(state, action) {
+      const content = action.payload;
+      state.push({
+        content,
+        important: false,
+        id: generateId(),
+      });
+    },
+    toggleImportanceOf(state, action) {
+      const id = action.payload;
       const noteToChange = state.find((n) => n.id === id);
       const changedNote = {
         ...noteToChange,
         important: !noteToChange.important,
       };
       return state.map((note) => (note.id !== id ? note : changedNote));
-    }
-    default:
-      return state;
-  }
-};
-
-// action creators
-const generateId = () => Number((Math.random() * 1000000).toFixed(0));
-
-export const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId(),
     },
-  };
-};
+  },
+});
 
-export const toggleImportanceOf = (id) => {
-  return {
-    type: 'TOGGLE_IMPORTANCE',
-    data: { id },
-  };
-};
-
-export default noteReducer;
+export const { createNote, toggleImportanceOf } = noteSlice.actions;
+export default noteSlice.reducer;
