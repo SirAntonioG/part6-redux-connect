a8f0cf763c49dd0b96a72b6510b893848ed1b8fb
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-15 22:39:44 -0500

    Fullstack part6 section c async actions

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import App from './App';
-
-// import { createStore, combineReducers } from 'redux';
-import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import noteReducer, { setNotes } from './reducers/noteReducer';
-import filterReducer from './reducers/filterReducer';
-import noteService from './services/note';
-
-const store = configureStore({
-  reducer: {
-    notes: noteReducer,
-    filter: filterReducer,
-  },
-});
-
-noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
-
-console.log(store.getState());
+import App from './App';
+import store from './store';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
