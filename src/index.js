8ab5aa3e2fa958f37c96e06e17658a2dcfb24cd0
AntonGreg <antonio.pena@epn.edu.ec>
2022-09-15 22:13:52 -0500

    Fullstack part6 section c comunicating with server

--- src/index.js
+++ src/index.js
@@ -6,8 +6,9 @@ import App from './App';
 // import { createStore, combineReducers } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import noteReducer from './reducers/noteReducer';
+import noteReducer, { setNotes } from './reducers/noteReducer';
 import filterReducer from './reducers/filterReducer';
+import noteService from './services/note';
 
 const store = configureStore({
   reducer: {
@@ -15,6 +16,8 @@ const store = configureStore({
     filter: filterReducer,
   },
 });
+
+noteService.getAll().then((notes) => store.dispatch(setNotes(notes)));
 
 console.log(store.getState());
 
